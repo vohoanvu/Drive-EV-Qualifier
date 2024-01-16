3bb131100ffb564847b7eab8d5ab1af4a660187b
Vu Vo <vohoanvu96@gmail.com>
2024-01-16 18:10:14 +0700

    form Poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FormComponent from './components/FormComponent';
 import ResultComponent from './components/ResultComponent';
 
@@ -9,27 +8,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Routes>
+            <Route path="/" element={<FormComponent/>} />
+            <Route path="/result" element={<ResultComponent/>} />
+          </Routes>
+        </Router>
       </header>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={FormComponent} />
-          <Route path="/result" component={ResultComponent} />
-        </Switch>
-      </Router>
     </div>
   );
 }
 
 export default App;
+
+/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.tsx</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */
