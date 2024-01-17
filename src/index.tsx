cbdc8c0c7d7f9569edd3781ffd8c26bccaa45a0c
Vu Vo <vohoanvu96@gmail.com>
2024-01-18 00:45:29 +0700

    removed strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>]
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>]
+      <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
